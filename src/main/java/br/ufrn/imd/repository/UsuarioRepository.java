999abf57ab50ad97a789c63ffc06d19185cd9b5f
Cleiton Lima <cleiton.lima@qualinfo.com.br>
2022-11-17 22:29:46 -0300

    Inserção da dependência do JPA

--- src/main/java/br/ufrn/imd/repository/UsuarioRepository.java
+++ src/main/java/br/ufrn/imd/repository/UsuarioRepository.java
@@ -4,4 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.ufrn.imd.model.Usuario;
 
+//usa o JpaRepository para fazer as operações de CRUD
+
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {}
