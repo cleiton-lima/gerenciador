7457e920795cfd52ea02d5dfa29d5b832efb63d5
mvinnicius22 <mvinnicius22@hotmail.com>
2022-12-02 20:28:18 -0300

    fix: enabling CORS

--- src/main/java/br/ufrn/imd/controller/ComentarioController.java
+++ src/main/java/br/ufrn/imd/controller/ComentarioController.java
@@ -8,6 +8,7 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -20,6 +21,7 @@ import br.ufrn.imd.model.Comentario;
 import br.ufrn.imd.repository.ComentarioRepository;
 
 @RestController
+@CrossOrigin
 public class ComentarioController {
 	
 	@Autowired
